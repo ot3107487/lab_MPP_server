9a3703fc869a2caf9cde76d50d504c6aebf4d4e3
ot3107487 <bogdan_danut_11@yahoo.com>
2018-05-02 02:29:21 +0300

    Main features implemented. Everything working fine from C# client. TO DO: notify clients

--- src/main/java/server/ServerGrpcImpl.java
+++ src/main/java/server/ServerGrpcImpl.java
@@ -1,18 +1,17 @@
 package server;
 
-import generatedFiles.services.Request;
-import generatedFiles.services.RequestType;
-import generatedFiles.services.Response;
-import generatedFiles.services.ServerImplementationGrpc;
 import io.grpc.stub.StreamObserver;
 import model.Artist;
+import model.Concert;
 import model.Location;
 import model.Ticket;
 import service.ConcertService;
 import service.LoginService;
 import service.Service;
+import ursus.GrpcProtos;
+import ursus.IServerGrpc;
 
-public class ServerGrpcImpl extends ServerImplementationGrpc.ServerImplementationImplBase {
+public class ServerGrpcImpl extends IServerGrpc.IServerImplBase {
     private final LoginService loginService;
     private final Service<Integer, Artist> artistService;
     private final Service<Integer, Location> locationService;
@@ -29,80 +28,113 @@ public class ServerGrpcImpl extends ServerImplementationGrpc.ServerImplementatio
     }
 
     @Override
-    public void clientRequest(Request request, StreamObserver<Response> responseObserver) {
-        if (request.getRequestType().equals(RequestType.LOGIN)) {
-            String body = request.getBodyAsString();
-            String[] credentials = body.split(" ");
-            String userName = credentials[0];
-            String password = credentials[1];
-            System.out.println("Sending response");
-            String status = Boolean.toString(loginService.login(userName, password));
-            Response response = Response.newBuilder().setStatus(status).build();
-            responseObserver.onNext(response);
+    public void login(GrpcProtos.CredentialsGrpc request, StreamObserver<GrpcProtos.GrpcReply> responseObserver) {
+        String password = request.getPassword();
+        String username = request.getUsername();
+        if (loginService.login(username, password)) {
+            responseObserver.onNext(GrpcProtos.GrpcReply.newBuilder().setStatus("OK").build());
             responseObserver.onCompleted();
-
+        } else {
+            responseObserver.onNext(GrpcProtos.GrpcReply.newBuilder().setStatus("FAILED").build());
+            responseObserver.onCompleted();
         }
-//            if (request.getRequestType().equals(RequestType.UPDATES)) {
-//                clients.add(out);
-//            }
-//            if (request.getRequestType().equals(RequestType.GET_CONCERTS_BY_ARTIST)) {
-//                Artist artist = (Artist) request.getBody();
-//                System.out.println("Sending response");
-//                out.writeObject(new Response(concertService.getConcertsByArtist(artist)));
-//                out.flush();
-//            }
-//            if (request.getRequestType().equals(RequestType.GET_CONCERTS_BY_DATE)) {
-//                String date = (String) request.getBody();
-//                System.out.println("Sending response");
-//                out.writeObject(new Response(concertService.getConcertsByDate(date)));
-//                out.flush();
-//            }
-//            if (request.getRequestType().equals(RequestType.GET_CONCERTS_BY_ARTIST_AND_DATE)) {
-//                WrapperArtistAndDate wrapperArtistAndDate = (WrapperArtistAndDate) request.getBody();
-//                Artist artist = wrapperArtistAndDate.getArtist();
-//                String date = wrapperArtistAndDate.getDate();
-//                System.out.println("Sending response");
-//                out.writeObject(new Response(concertService.getConcertsByArtistAndDate(artist, date)));
-//                out.flush();
-//            }
-//            if (request.getRequestType().equals(RequestType.SAVE_TICKET)) {
-//                Ticket ticket = (Ticket) request.getBody();
-//                System.out.println("Sending response");
-//                ticketService.save(ticket);
-//                out.writeObject(new Response(null));
-//                out.flush();
-//            }
-//            if (request.getRequestType().equals(RequestType.UPDATE_CONCERT)) {
-//                Concert concert = (Concert) request.getBody();
-//                System.out.println("Sending response");
-//                concertService.put(concert);
-//                out.writeObject(new Response(null));
-//                out.flush();
-//                System.out.println("Sending updates");
-//
-//
-//                //########## UPDATEING OTHER CLIENTS
-//                for (ObjectOutputStream cl : clients) {
-//                    System.out.println("Sending update to:"+ cl);
-//                    cl.flush();
-//                    cl.writeObject(new Response(concert));
-//                    cl.flush();
-//                }
-//            }
-//            if (request.getRequestType().equals(RequestType.GET_ARTISTS)) {
-//                System.out.println("Sending response");
-//                out.writeObject(new Response(artistService.getAll()));
-//                out.flush();
-//            }
-//            if (request.getRequestType().equals(RequestType.GET_CONCERTS)) {
-//                System.out.println("Sending response");
-//                out.writeObject(new Response(concertService.getAll()));
-//                out.flush();
-//            }
-//            if (request.getRequestType().equals(RequestType.GET_LOCATIONS)) {
-//                System.out.println("Sending response");
-//                out.writeObject(new Response(locationService.getAll()));
-//                out.flush();
-//            }
+    }
+
+    @Override
+    public void getArtists(GrpcProtos.RequestGrpc request, StreamObserver<GrpcProtos.ArtistList> responseObserver) {
+        GrpcProtos.ArtistList.Builder artistList = GrpcProtos.ArtistList.newBuilder();
+        for (Artist artist : artistService.getAll()) {
+            GrpcProtos.ArtistGrpc.Builder artistBuilder = GrpcProtos.ArtistGrpc.newBuilder();
+            artistBuilder.setFirstname(artist.getFirstName());
+            artistBuilder.setLastname(artist.getLastName());
+            artistBuilder.setId(artist.getId());
+            artistList.addArtists(artistBuilder.build());
+        }
+        responseObserver.onNext(artistList.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getConcerts(GrpcProtos.RequestGrpc request, StreamObserver<GrpcProtos.ConcertList> responseObserver) {
+        GrpcProtos.ConcertList.Builder concertList = GrpcProtos.ConcertList.newBuilder();
+        for (Concert concert : concertService.getAll()) {
+            GrpcProtos.ConcertGrpc.Builder concertBuilder = GrpcProtos.ConcertGrpc.newBuilder();
+            concertBuilder.setDate(concert.getDate());
+            concertBuilder.setId(concert.getId());
+            concertBuilder.setIdArtist(concert.getIdArtist());
+            concertBuilder.setIdLocation(concert.getIdLocation());
+            concertBuilder.setNumberOfTickets(concert.getNumberOfTickets());
+            concertBuilder.setSoldTickets(concert.getSoldTickets());
+            concertList.addConcerts(concertBuilder.build());
+        }
+        responseObserver.onNext(concertList.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getLocation(GrpcProtos.RequestGrpc request, StreamObserver<GrpcProtos.LocationList> responseObserver) {
+        GrpcProtos.LocationList.Builder locationList = GrpcProtos.LocationList.newBuilder();
+        for (Location location : locationService.getAll()) {
+            GrpcProtos.LocationGrpc.Builder locationBuilder = GrpcProtos.LocationGrpc.newBuilder();
+            locationBuilder.setId(location.getId());
+            locationBuilder.setName(location.getName());
+            locationBuilder.setPlaces(location.getPlaces());
+            locationList.addLocations(locationBuilder.build());
+        }
+        responseObserver.onNext(locationList.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getConcertsByArtist(GrpcProtos.ArtistGrpc request, StreamObserver<GrpcProtos.ConcertList> responseObserver) {
+        Artist artist = new Artist(request.getId(), request.getLastname(), request.getFirstname());
+        GrpcProtos.ConcertList.Builder concertList = GrpcProtos.ConcertList.newBuilder();
+        for (Concert concert : concertService.getConcertsByArtist(artist)) {
+            GrpcProtos.ConcertGrpc.Builder concertBuilder = GrpcProtos.ConcertGrpc.newBuilder();
+            concertBuilder.setDate(concert.getDate());
+            concertBuilder.setId(concert.getId());
+            concertBuilder.setIdArtist(concert.getIdArtist());
+            concertBuilder.setIdLocation(concert.getIdLocation());
+            concertBuilder.setNumberOfTickets(concert.getNumberOfTickets());
+            concertBuilder.setSoldTickets(concert.getSoldTickets());
+            concertList.addConcerts(concertBuilder.build());
+        }
+        responseObserver.onNext(concertList.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getConcertsByDate(GrpcProtos.RequestGrpc request, StreamObserver<GrpcProtos.ConcertList> responseObserver) {
+        String date = request.getRequestType();
+        GrpcProtos.ConcertList.Builder concertList = GrpcProtos.ConcertList.newBuilder();
+        for (Concert concert : concertService.getConcertsByDate(date)) {
+            GrpcProtos.ConcertGrpc.Builder concertBuilder = GrpcProtos.ConcertGrpc.newBuilder();
+            concertBuilder.setDate(concert.getDate());
+            concertBuilder.setId(concert.getId());
+            concertBuilder.setIdArtist(concert.getIdArtist());
+            concertBuilder.setIdLocation(concert.getIdLocation());
+            concertBuilder.setNumberOfTickets(concert.getNumberOfTickets());
+            concertBuilder.setSoldTickets(concert.getSoldTickets());
+            concertList.addConcerts(concertBuilder.build());
+        }
+        responseObserver.onNext(concertList.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void saveTicket(GrpcProtos.TicketGrpc request, StreamObserver<GrpcProtos.GrpcReply> responseObserver) {
+        Ticket ticket = new Ticket(request.getId(), request.getIdConcert(), request.getPlacesBought(), request.getBuyer());
+        ticketService.save(ticket);
+        responseObserver.onNext(GrpcProtos.GrpcReply.newBuilder().build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateConcert(GrpcProtos.ConcertGrpc request, StreamObserver<GrpcProtos.GrpcReply> responseObserver) {
+        Concert concert=new Concert(request.getId(),request.getIdArtist()
+                ,request.getDate(),request.getIdLocation(),request.getNumberOfTickets(),request.getSoldTickets());
+        concertService.put(concert);
+        responseObserver.onNext(GrpcProtos.GrpcReply.newBuilder().build());
+        responseObserver.onCompleted();
     }
 }
